8557604ef94f43d0e855fd4df3c1608b6066dfea
titaszev <57100578+titaszev@users.noreply.github.com>
2023-07-15 21:12:28 +0300

    made filters cooler svg's need fixing

--- Play/Agents/roulette/script.js
+++ Play/Agents/roulette/script.js
@@ -1,10 +1,5 @@
 const Agents = document.getElementsByClassName("agent-card");
 
-const controllers = document.getElementsByClassName("controllers");
-const duelists = document.getElementsByClassName("duelists");
-const initiators = document.getElementsByClassName("initiators");
-const sentinels = document.getElementsByClassName("sentinels");
-
 function lock(evt) {
   evt.currentTarget.classList.toggle("locked");
 }
@@ -47,15 +42,57 @@ function reset() {
   });
 }
 
+//elements
+
+const controllers = document.getElementsByClassName("controllers");
+const duelists = document.getElementsByClassName("duelists");
+const initiators = document.getElementsByClassName("initiators");
+const sentinels = document.getElementsByClassName("sentinels");
+
 //filters
 
-console.log(controllers);
-console.log(duelists);
-console.log(initiators);
-console.log(sentinels);
+// console.log(controllers);
+// console.log(duelists);
+// console.log(initiators);
+// console.log(sentinels);
+
+//add
 
 function lockElements(elements) {
   for (let i = 0; i < elements.length; i++) {
-    elements[i].classList.toggle("locked");
+    elements[i].classList.add("locked");
+  }
+}
+
+//remove
+
+function unlockElements(elements) {
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].classList.remove("locked");
+  }
+}
+
+// filters
+
+const filterControllers = document
+  .getElementsByClassName("lockControllers")
+  .item(0);
+const filterDuelists = document.getElementsByClassName("lockDuelists").item(0);
+const filterInitiators = document
+  .getElementsByClassName("lockInitiators")
+  .item(0);
+const filterSentinels = document
+  .getElementsByClassName("lockSentinels")
+  .item(0);
+
+//function filter
+
+function filter(elements, filterClass) {
+  if (filterClass.classList.contains("inactive")) {
+    filterClass.classList.replace("inactive", "active");
+    lockElements(elements);
+  } else {
+    filterClass.classList.replace("active", "inactive");
+    unlockElements(elements);
   }
 }
